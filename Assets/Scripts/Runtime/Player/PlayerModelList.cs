6c0ca4d312130dc517f20ab514600229cf9e2b5a
Cocomineral <tintinch59@gmail.com>
2023-10-10 16:15:25 +0200

    [ADD] IEnumerable IPlayerModel in PlayerModelList Script

--- Assets/Scripts/Runtime/Player/PlayerModelList.cs
+++ Assets/Scripts/Runtime/Player/PlayerModelList.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 namespace Game
 {
+    [CreateAssetMenu(menuName = "ScriptableObjects/Player Model List")]
     public class PlayerModelList : ReadOnlyPlayerModelList
     {
         private List<IPlayerModel> _mPlayers = new();
 
-        public List<IPlayerModel> Players => _mPlayers;
-        public override IReadOnlyList<IPlayerModel> PlayerModels => _mPlayers;
+        public List<IPlayerModel> Players
+        {
+            get => _mPlayers;
+            set => _mPlayers = value;
+        }
+
+        public PlayerModelList(IEnumerable<IPlayerModel> players) : base(players)
+        {
+        }
     }
 }
-
--- Assets/Scripts/Runtime/Player/ReadOnlyPlayerModelList.cs
+++ Assets/Scripts/Runtime/Player/ReadOnlyPlayerModelList.cs
@@ -3,7 +3,27 @@ using System.Collections.Generic;
 using Game;
 using UnityEngine;
 
-public abstract class ReadOnlyPlayerModelList : ScriptableObject
+namespace Game
 {
-    public abstract IReadOnlyList<IPlayerModel> PlayerModels { get; }
+    public abstract class ReadOnlyPlayerModelList : ScriptableObject, IEnumerable<IPlayerModel>
+    {
+        private List<IPlayerModel> _players;
+
+        public ReadOnlyPlayerModelList(IEnumerable<IPlayerModel> players)
+        {
+            _players = new List<IPlayerModel>(players);
+        }
+
+        public IReadOnlyList<IPlayerModel> PlayerModels => _players.AsReadOnly();
+
+        public IEnumerator<IPlayerModel> GetEnumerator()
+        {
+            return _players.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _players.GetEnumerator();
+        }
+    }
 }
