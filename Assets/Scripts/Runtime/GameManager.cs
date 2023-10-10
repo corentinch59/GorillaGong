5709bd2b79325fbb0b9129cf204f0d1dcdc83c85
Outfoxeed <letourneaumorigan@gmail.com>
2023-10-10 10:21:20 +0200

    [ADD] GameManager

---
+++ Assets/Scripts/Runtime/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Runtime.GameModes;
+using Runtime.GameModes.Config;
+using Runtime.GameModes.Factory;
+using UnityEngine;
+
+namespace Game.Runtime
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private PlayerManager _playerManager;
+        [SerializeField] private GameModeConfigDictionary _gameModeConfigs;
+        private IGameModeFactory _gameModeFactory;
+
+        private IGameMode _currentGameMode;
+
+        private void Awake()
+        {
+            _gameModeFactory = new GameModeFactory(_playerManager, _gameModeConfigs);
+            _currentGameMode = _gameModeFactory.Create(GameModeType.Simple);
+            _currentGameMode.Start();
+        }
+    }
+}
