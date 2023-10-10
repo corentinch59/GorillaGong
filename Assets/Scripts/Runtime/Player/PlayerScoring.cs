6c9434f15ad885f486833778df1ef76e0c3fb0a9
Outfoxeed <letourneaumorigan@gmail.com>
2023-10-10 11:26:43 +0200

    [UPDATE] Player scores clamped

--- Assets/Scripts/Runtime/Player/PlayerScoring.cs
+++ Assets/Scripts/Runtime/Player/PlayerScoring.cs
@@ -1,20 +1,23 @@
-﻿using UniRx;
+﻿using ScriptableObjectArchitecture;
+using UniRx;
+using UnityEngine;
 
 namespace Game
 {
     public partial class Player
     {
-        public IReadOnlyReactiveProperty<int> Score => _score;
-        private ReactiveProperty<int> _score = new();
+        [SerializeField] private FloatVariable _maxScore;
+        public IReadOnlyReactiveProperty<float> Score => _score;
+        private ReactiveProperty<float> _score = new();
 
-        public void AddScore(int amount)
+        public void AddScore(float amount)
         {
-            _score.Value += amount;
+            _score.Value = Mathf.Min(_maxScore.Value, _score.Value + amount);
         }
 
-        public void RemoveScore(int amount)
+        public void RemoveScore(float amount)
         {
-            _score.Value -= amount;
+            _score.Value = Mathf.Max(0, _score.Value - amount);
         }
     }
 }
