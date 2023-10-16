1446bea224fa2a1872a5b690a15ada2a8bd9cf07
Outfoxeed <letourneaumorigan@gmail.com>
2023-10-16 23:36:49 +0200

    [ADD] InputCallback MonoBehaviour

---
+++ Assets/Scripts/Runtime/Inputs/InputCallback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace GorillaGong.Runtime.Inputs
+{
+    public class InputCallback : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference _inputActionRef;
+        [SerializeField] private UnityEvent _response;
+        
+        private void OnEnable()
+        {
+            _inputActionRef.action.performed += OnInputActionPerformed;
+        }
+
+        private void OnDisable()
+        {
+            _inputActionRef.action.performed -= OnInputActionPerformed;
+        }
+
+        private void OnInputActionPerformed(InputAction.CallbackContext obj)
+        {
+            _response.Invoke();
+        }
+    }
+}
