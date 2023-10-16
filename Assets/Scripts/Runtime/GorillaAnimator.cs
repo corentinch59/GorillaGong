a1aca14db6ed47fe20f66589186a7e34c14d7f00
Outfoxeed <letourneaumorigan@gmail.com>
2023-10-16 23:37:59 +0200

    [ADD] GorillaAnimator MonoBehaviour

---
+++ Assets/Scripts/Runtime/GorillaAnimator.cs
@@ -0,0 +1,33 @@
+﻿using GorillaGong.Runtime.Inputs;
+using UnityEngine;
+
+namespace GorillaGong.Runtime
+{
+    public class GorillaAnimator : MonoBehaviour
+    {
+        [SerializeField] private int _playerIndex;
+        [SerializeField] private PlayerInputGameEvent _playerInputEvent;
+
+        [Header("Animator")] 
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string[] _animatorTriggersName;
+
+        private void OnEnable()
+        {
+            _playerInputEvent.AddListener(OnPlayerInputEvent);
+        }
+
+        private void OnPlayerInputEvent(PlayerInput playerInput)
+        {
+            if (_playerIndex != playerInput.PlayerModel.Index)
+            {
+                return;
+            }
+
+            foreach (int playerInputId in playerInput.InputIds)
+            {
+                _animator.SetTrigger(_animatorTriggersName[playerInputId]);
+            }
+        }
+    }
+}
