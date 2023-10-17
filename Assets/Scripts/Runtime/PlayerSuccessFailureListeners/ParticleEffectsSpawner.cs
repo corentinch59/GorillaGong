025f789790f9c3abcbf3859c840b23e89c85fd12
Outfoxeed <letourneaumorigan@gmail.com>
2023-10-17 09:54:37 +0200

    [ADD] PlayerSucessF/Failure Listeners + ParticleEffectsSpawner

---
+++ Assets/Scripts/Runtime/PlayerSuccessFailureListeners/ParticleEffectsSpawner.cs
@@ -0,0 +1,42 @@
+﻿using GorillaGong.Runtime.Player;
+using UnityEngine;
+
+namespace GorillaGong.Runtime
+{
+    public class ParticleEffectsSpawner : PlayerSuccessFailureListener
+    {
+        [Header("Particles")]
+        [SerializeField] private Transform[] _spawnPositions;
+        [SerializeField] private GameObject _successParticlesPrefab;
+        [SerializeField] private GameObject _failParticlesPrefab;
+        [SerializeField] private float _particlesDestroyTime = 2f;
+        
+        protected override void OnPlayerSuccess(IPlayerModel player)
+        {
+            if (player.Index != PlayerIndex)
+            {
+                return;
+            }
+            
+            SpawnParticle(_successParticlesPrefab);
+        }
+
+        protected override void OnPlayerFail(IPlayerModel player)
+        {
+            if (player.Index != PlayerIndex)
+            {
+                return;
+            }
+            
+            SpawnParticle(_failParticlesPrefab);
+        }
+
+        private void SpawnParticle(GameObject particlePrefab)
+        {
+            foreach (int targets in PlayerPatterns.Values[PlayerIndex].Values)
+            {
+                Destroy(GameObject.Instantiate(particlePrefab, _spawnPositions[targets], true), _particlesDestroyTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/PlayerSuccessFailureListeners/PlayerSuccessFailureListener.cs
@@ -0,0 +1,29 @@
+﻿using GorillaGong.Runtime.GameEvents;
+using GorillaGong.Runtime.Player;
+using UnityEngine;
+
+namespace GorillaGong.Runtime
+{
+    public abstract class PlayerSuccessFailureListener : MonoBehaviour
+    {
+        [field: SerializeField] protected int PlayerIndex { get; private set; }
+        [field: SerializeField] protected PlayerPatterns.PlayerPatterns PlayerPatterns { get; private set; }
+        [SerializeField] private PlayerModelGameEvent _playerSuccessEvent;
+        [SerializeField] private PlayerModelGameEvent _playerFailEvent;
+
+        private void OnEnable()
+        {
+            _playerSuccessEvent.AddListener(OnPlayerSuccess);
+            _playerFailEvent.AddListener(OnPlayerFail);
+        }
+
+        private void OnDisable()
+        {
+            _playerSuccessEvent.RemoveListener(OnPlayerSuccess);
+            _playerFailEvent.RemoveListener(OnPlayerFail);
+        }
+
+        protected abstract void OnPlayerSuccess(IPlayerModel player);
+        protected abstract void OnPlayerFail(IPlayerModel player);
+    }
+}
