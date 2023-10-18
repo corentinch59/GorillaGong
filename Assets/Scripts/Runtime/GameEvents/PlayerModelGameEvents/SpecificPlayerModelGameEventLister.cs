13688efd0a434fc08a917676c0b71fbe326c5a68
Outfoxeed <letourneaumorigan@gmail.com>
2023-10-18 10:11:37 +0200

    [ADD] Gorilla Scale on Victory/Loose

---
+++ Assets/Scripts/Runtime/GameEvents/PlayerModelGameEvents/SpecificPlayerModelGameEventLister.cs
@@ -0,0 +1,25 @@
+﻿using GorillaGong.Runtime.Player;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GorillaGong.Runtime
+{
+    public class SpecificPlayerModelGameEventLister : PlayerModelGameEventListener
+    {
+        [SerializeField] private int _playerIndex;
+        [SerializeField] private UnityEvent _winResponse;
+        [SerializeField] private UnityEvent _loseResponse;
+
+        protected override void GameFinished(IPlayerModel player)
+        {
+            if (player.Index == _playerIndex)
+            {
+                _winResponse.Invoke();
+            }
+            else
+            {
+                _loseResponse.Invoke();
+            }
+        }
+    }
+}
